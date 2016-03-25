dbf1d298a298dc9a875070f79f10a2ba2f21d5f3
EKR <ekr@rtfm.com>
2016-03-25 13:14:09 -0700

    potential fix

--- id.mk
+++ id.mk
@@ -33,7 +33,12 @@ CI_REPO = $(word 2,$(subst /, ,$(TRAVIS_REPO_SLUG)))$(CIRCLE_PROJECT_REPONAME)
 ifeq (true,$(CI))
 CI_REPO_FULL = $(CI_USER)/$(CI_REPO)
 endif
-CI_IS_PR = $(if $(CI_PULL_REQUESTS),true,$(ifeq(false, $(TRAVIS_PULL_REQUEST)),false,true))
+ifeq (false,$(TRAVIS_PULL_REQUEST))
+CI_TRAVIS_PR = false
+else
+CI_TRAVIS_PR = true
+endif
+CI_IS_PR = $(if $(CI_PULL_REQUESTS),true,$(CI_TRAVIS_PR))
 
 # Github guesses
 ifndef CI_REPO_FULL
